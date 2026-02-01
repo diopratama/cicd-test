d13495b4a59c5503e77988a5fddf0cca7ac7bef3
Ideadio <dio.pratama@grabtaxi.com>
2026-02-01 21:24:25 +0700

    change-1

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World! Deploy via CI/CD Berhasil!\n');
+    res.end('Hello World! Deploy via CI/CD Berhasil!\n Ini adalah perubahan ke-1');
 });
 server.listen(3000, () => console.log('Server running on port 3000'));
